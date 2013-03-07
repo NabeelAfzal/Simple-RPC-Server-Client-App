fb98289671471af7b142f1882583fbd7274c2fd9
Nabeel <nabeel@xgridtech.com>
2013-03-07 23:09:51 +0500

    Updated source files.

--- src/compare.c
+++ src/compare.c
@@ -4,7 +4,19 @@
 #include <string.h>
 #include "compare.h"
 
-// Compare both strings to check for match.
+/************************************************************************
+*  
+* @purpose function to compare two strings to check for a match
+*
+* @param const char*    str       String containing data read from config file
+* @param const char*    prefix    String containing the text with which the data
+*                                 is being matched
+*    
+* @returns int    0 (SUCCESS) or 1 (FAILURE)
+* @notes none
+* @end
+*
+***********************************************************************/
 int starts_with(const char *str, const char *prefix) {
   if (memcmp(str, prefix, strlen(prefix)) == 0)
     return 1;
